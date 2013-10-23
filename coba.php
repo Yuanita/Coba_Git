beb94c54c47f6219b4eea601b65bb05714c8297d
Yuanita <yuanita.annisa@gmail.com>
2013-10-23 04:46:25 -0700

    ver2
    perubahan coding

--- coba.php
+++ coba.php
@@ -1,1 +1,15 @@
-coba
+#include
+#include <srting.h>
+
+int main (int argc, char ** argv)
+{
+	char nama [255];
+	
+	printf ("Masukkan Nama Anda :");
+	fgets (nama, 255, stdin);
+	printf ("panjang =% d \ n", strlen (nama)); / * debug baris */
+	Nama [strlen (nama) -1] = '\0'; /* menghapus baris diakhir */
+	
+	printf ("Hello% s \ n", nama);
+	return 0;
+}
