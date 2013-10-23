f990f59a76195ef6fa06c09e3b41887adf3d29ff
Yuanita <yuanita.annisa@gmail.com>
2013-10-23 04:50:47 -0700

    ver3
    penambahan nama belakang

--- coba.php
+++ coba.php
@@ -5,11 +5,13 @@ int main (int argc, char ** argv)
 {
 	char nama [255];
 	
-	printf ("Masukkan Nama Anda :");
+	printf ("Masukkan Nama pertama Anda :");
 	fgets (nama, 255, stdin);
-	printf ("panjang =% d \ n", strlen (nama)); / * debug baris */
-	Nama [strlen (nama) -1] = '\0'; /* menghapus baris diakhir */
+	pertama [strlen (pertama) -1] = '\0'; /* menghapus baris diakhir */
 	
-	printf ("Hello% s \ n", nama);
+	printf ("Sekarang masukkan Nama belakang Anda :");
+	mendapat (terakhir); / * buffer overlow? apa itu ? * /
+	
+	printf ("Hello% s \ n", pertama, terakhir);
 	return 0;
 }
